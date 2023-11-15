32a6715f663d1305ba86881d06ffb489eafaaade
sekler09 <seklertyoma@gmail.com>
2023-11-15 19:32:19 +0300

    feat: init postgresql connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,8 +5,32 @@ import { AuthModule } from './auth/auth.module';
 import { HotelModule } from './hotel/hotel.module';
 import { RoomModule } from './room/room.module';
 import { ReviewModule } from './review/review.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [AuthModule, UserModule, HotelModule, RoomModule, ReviewModule],
+  imports: [
+    AuthModule,
+    UserModule,
+    HotelModule,
+    RoomModule,
+    ReviewModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        synchronize: true,
+        entities: [__dirname + '/**/entities/*.entity.ts'],
+        logging: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class AppModule {}
